b36697bade1e4b44fe8671ca6fc99c16e4a030dd
Max199619 <66778549+Max199619@users.noreply.github.com>
2021-11-12 01:57:55 +0100

    added javaDocs

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,15 +4,30 @@ import java.util.Random;
 
 public class Main {
 
+    /**
+     * random number, which player aims to guess
+     */
     public static int randNum;
+    /**
+     * did the player guess correctly?
+     */
     public static boolean correctGuess;
 
     public static void main(String[] args){
+        // New player starts a game
         Player player = new Player();
         player.play = true;
+        //Repeat until Player says, he doesn't want to play again
         while (player.play){
+            //Creating new Random number
             NewRandom();
+            /*
+            When a new random number is created, the variable, checking
+            if the input is correct is set to false - incorrect, as a prime
+            point
+             */
             correctGuess = false;
+            //Repeat until the Player guesses correctly
             while (!correctGuess){
                 player.NewGuess();
                 correctGuess = player.CheckGuess(randNum);
@@ -25,6 +40,9 @@ public class Main {
     }
 
 
+    /**
+     * Creates a random number in range 1-100 (inclusive)
+     */
     public static void NewRandom(){
         Random rand = new Random();
         randNum = rand.nextInt(100);
--- src/com/company/Player.java
+++ src/com/company/Player.java
@@ -4,9 +4,21 @@ import java.util.Scanner;
 
 public class Player {
 
+    /**
+     * number of guesses made by a player
+     */
     private int guesses;
+    /**
+     * checks if the user still wants to play
+     */
     public boolean play;
+    /**
+     * a number input by a player as a guess
+     */
     private int guess;
+    /**
+     * class used to get the player's input
+     */
     public Scanner sc = new Scanner(System.in);
 
     public Player(){
@@ -29,18 +41,29 @@ public class Player {
         this.guess = guess;
     }
 
+    /**
+     * Lets player decide if player wants to play again, by giving input
+     */
     public void PlayAgain(){
         System.out.println("Would you like to play again? (y/n) ");
         String answer = sc.next();
         play = answer.equals("y");
     }
 
+    /**
+     * Takes user input, sets it as player's guess and increments
+     * the number of player's guesses
+     */
     public void NewGuess(){
         System.out.print("Make your guess: ");
         this.setGuess(sc.nextInt());
         this.setGuesses(this.getGuesses() + 1);
     }
 
+    /**
+     * @param randNum - random number created in the main class
+     * @return correct - is the guess made by player correct
+     */
     public boolean CheckGuess(int randNum){
         boolean correct;
         if (this.getGuess() == randNum){
